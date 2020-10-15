b270d3dda1b8294c5da6d00778067d0ae16b11a2
anikett-rpx <anikett_c@rpxcorp.com>
2020-10-15 15:10:41 +0530

    [21] Basics for form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import StyleSheet from './StyleSheet';
 import Inline from './Inline';
 import './appStyle.css';
 import styles from './appStyle.module.css'
+import Form from './Form';
 
 function App() {
   return (
@@ -21,13 +22,16 @@ function App() {
       {/* <ParentComponent/> */}
       {/* <UserGreeting/> */}
       {/* <NameList/> */}
+      
       {/* <StyleSheet enabled={true}/> */}
       {/* <Inline/> */}
-      <h1 className='error'>Hello</h1>
+      {/* <h1 className='error'>Hello</h1> */}
       {/*Module css will help in resolving name conflict. For example the error class if 
       we place in Inline Component in future it can give css conflict. If you try to copy
       the module css in the other component it will throw an error*/}
-      <h1 className={styles.success}>How are you</h1>
+      {/* <h1 className={styles.success}>How are you</h1> */}
+
+      <Form/>
     </>
   );
 }
---
+++ src/Form.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            comment: '',
+            topic: 'react'
+        }
+    }
+
+    handleUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleComment = (event) => {
+        this.setState({
+            comment: event.target.value
+        })
+    }
+
+    handleLanguange = (event) => {
+        this.setState({
+            topic: event.target.value
+        })
+    }
+
+    handleForm = (event) => {
+        alert(`${this.state.username} ${this.state.comment} ${this.state.topic}`);
+        event.preventDefault()
+    }
+
+    render() {
+        return(
+            <>
+                <form onSubmit={this.handleForm}>
+                    <div>
+                      <label>Username</label>
+                      <input type="text" value={this.state.username} 
+                    onChange={this.handleUsername} />
+                    </div>
+                    <div>
+                      <label>Comments</label>
+                      <input type="text" value={this.state.comment} 
+                    onChange={this.handleComment} />
+                    </div>
+                    <div>
+                        <label>Language</label>
+                        <select value={this.state.topic} onChange={this.handleLanguange}>
+                            <option value="react">React</option>
+                            <option value="vue">Vue</option>
+                            <option value="angular">Angular</option>
+                        </select>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </>
+        )
+    }
+}
+
+export default Form;
